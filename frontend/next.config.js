9dfd4ec5d032831f92419071c3623e3fdc4ce789
Bhavya Agarwal <bhavya@coinfeeds.io>
2026-03-28 22:58:46 -0700

    csp fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "img-src 'self' data: https:",
+              "connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://*.polymarket.com https://*.polyspotter.com",
+            ].join("; "),
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
